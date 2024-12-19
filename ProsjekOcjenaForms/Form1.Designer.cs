a635049f9e0b2620361c39001b2cc291a031353d
Mrkva77 <156481047+Mrkva77@users.noreply.github.com>
2024-12-19 09:38:43 +0100

    Add files via upload

---
+++ ProsjekOcjenaForms/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ProsjekOcjenaForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(167, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(212, 30);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(198, 26);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Predmet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Unos Ocjena";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Prosjek";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 306);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ProsjekOcjenaForms/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ProsjekOcjenaForms.ocjena;
+using static ProsjekOcjenaForms.prosjek;
+
+namespace ProsjekOcjenaForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private List<Ocjena> ocjene = new List<Ocjena>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int vrijednostOcjene = Convert.ToInt32(textBox1.Text);
+                if (vrijednostOcjene < 1 || vrijednostOcjene > 5)
+                {
+                    MessageBox.Show("Ocjena mora biti između 1 i 5.");
+                    return;
+                }
+
+                Predmet predmet = (Predmet)comboBox1.SelectedItem;
+
+                DateTime datum = dateTimePicker1.Value;
+
+                Ocjena novaOcjena = new Ocjena(predmet, vrijednostOcjene, datum);
+
+                ocjene.Add(novaOcjena);
+
+                MessageBox.Show("Ocjena uspješno unesena!");
+
+                IzracunajProsjek();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška pri unosu: " + ex.Message);
+            }
+        }
+
+        private void IzracunajProsjek()
+        {
+            if (ocjene.Count == 0)
+            {
+                MessageBox.Show("Nema unesenih ocjena.");
+                return;
+            }
+
+            double suma = 0;
+            foreach (Ocjena ocjena in ocjene)
+            {
+                suma += ocjena.Vrijednost;
+            }
+
+            double prosjek = suma / ocjene.Count;
+            label1.Text = "Prosjek: " + prosjek.ToString("F2");
+        }
+
+        private void SpremiOcjene()
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("ocjene.txt"))
+                {
+                    foreach (Ocjena ocjena in ocjene)
+                    {
+                        writer.WriteLine($"{ocjena.Predmet.Naziv_predmeta},{ocjena.Vrijednost},{ocjena.Datum.ToShortDateString()}");
+                    }
+                }
+                MessageBox.Show("Ocjene su uspješno spremljene.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška pri spremanju: " + ex.Message);
+            }
+        }
+
+        private void UcitajOcjene()
+        {
+            try
+            {
+                ocjene.Clear();
+                using (StreamReader reader = new StreamReader("ocjene.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var parts = line.Split(',');
+                        string nazivPredmeta = parts[0];
+                        int vrijednost = int.Parse(parts[1]);
+                        DateTime datum = DateTime.Parse(parts[2]);
+
+                        Predmet predmet = new Predmet(nazivPredmeta);
+                        Ocjena ocjena = new Ocjena(predmet, vrijednost, datum);
+                        ocjene.Add(ocjena);
+                    }
+                }
+                MessageBox.Show("Ocjene su uspješno učitane.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška pri učitavanju: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            comboBox1.Items.Add(new Predmet("Matematika"));
+            comboBox1.Items.Add(new Predmet("Povijest"));
+            comboBox1.Items.Add(new Predmet("Fizika"));
+            comboBox1.Items.Add(new Predmet("Hrvatski"));
+            comboBox1.Items.Add(new Predmet("Engleski"));
+            comboBox1.Items.Add(new Predmet("Talijanski"));
+            comboBox1.SelectedIndex = 0;
+        }
+
+    }
+
+}
